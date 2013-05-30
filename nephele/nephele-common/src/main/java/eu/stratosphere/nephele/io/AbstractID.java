ffa12fdd2a86d8756832c44f80f5413af99307da
arvid <arvid.heise@hpi.uni-potsdam.de>
2013-05-30 18:14:35 +0200

    Backported ID Methods from 0.2.1

--- nephele/nephele-common/src/main/java/eu/stratosphere/nephele/io/AbstractID.java
+++ nephele/nephele-common/src/main/java/eu/stratosphere/nephele/io/AbstractID.java
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
  *
- * Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -22,8 +22,10 @@ import java.io.IOException;
 import eu.stratosphere.nephele.util.StringUtils;
 
 /**
- * ID is an abstract base class to provide statistically unique and serializable identification numbers in Nephele.
+ * ID is an abstract base class for providing statistically unique identification numbers in Nephele.
  * Every component that requires these kinds of IDs provides its own concrete type.
+ * <p>
+ * This class is thread-safe.
  * 
  * @author warneke
  */
@@ -63,12 +65,67 @@ public abstract class AbstractID implements IOReadableWritable {
 	}
 
 	/**
-	 * Constructs a new random ID from a uniform distribution.
+	 * Constructs a new abstract ID.
+	 * 
+	 * @param lowerPart
+	 *        the lower bytes of the ID
+	 * @param upperPart
+	 *        the higher bytes of the ID
 	 */
-	public AbstractID() {
+	protected AbstractID(final long lowerPart, final long upperPart) {
 
-		this.lowerPart = (long) (Math.random() * Long.MAX_VALUE);
-		this.upperPart = (long) (Math.random() * Long.MAX_VALUE);
+		this.lowerPart = lowerPart;
+		this.upperPart = upperPart;
+	}
+
+	/**
+	 * Creates a new abstract ID from the given one. The given and the newly created abtract ID will be identical, i.e.
+	 * a comparison by <code>equals</code> will return <code>true</code> and both objects will have the same hash code.
+	 * 
+	 * @param id
+	 *        the abstract ID to copy
+	 */
+	protected AbstractID(final AbstractID id) {
+
+		this.lowerPart = id.lowerPart;
+		this.upperPart = id.upperPart;
+	}
+
+	/**
+	 * Default constructor required by kryo.
+	 */
+	protected AbstractID() {
+
+		this.lowerPart = 0L;
+		this.upperPart = 0L;
+	}
+
+	/**
+	 * Generates a uniformly distributed random positive long.
+	 * 
+	 * @return a uniformly distributed random positive long
+	 */
+	protected static long generateRandomBytes() {
+
+		return (long) (Math.random() * Long.MAX_VALUE);
+	}
+
+	/**
+	 * Converts a long to a byte array.
+	 * 
+	 * @param l
+	 *        the long variable to be converted
+	 * @param ba
+	 *        the byte array to store the result the of the conversion
+	 * @param offset
+	 *        the offset indicating at what position inside the byte array the result of the conversion shall be stored
+	 */
+	private static void longToByteArray(final long l, final byte[] ba, final int offset) {
+
+		for (int i = 0; i < SIZE_OF_LONG; ++i) {
+			final int shift = i << 3; // i * 8
+			ba[offset + SIZE_OF_LONG - 1 - i] = (byte) ((l & (0xffL << shift)) >>> shift);
+		}
 	}
 
 	/**
@@ -89,24 +146,6 @@ public abstract class AbstractID implements IOReadableWritable {
 		}
 
 		return l;
-	}
-
-	/**
-	 * Converts a long to a byte array.
-	 * 
-	 * @param l
-	 *        the long variable to be converted
-	 * @param ba
-	 *        the byte array to store the result the of the conversion
-	 * @param offset
-	 *        the offset indicating at what position inside the byte array the result of the conversion shall be stored
-	 */
-	private static void longToByteArray(final long l, final byte[] ba, final int offset) {
-
-		for (int i = 0; i < SIZE_OF_LONG; ++i) {
-			final int shift = i << 3; // i * 8
-			ba[offset + SIZE_OF_LONG - 1 - i] = (byte) ((l & (0xffL << shift)) >>> shift);
-		}
 	}
 
 	/**
